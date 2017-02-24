0c0aef0939618718de819bd8dae3eec36f1989c1
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-02-24 09:25:19 +0200

    Add CORS in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,12 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.middleware.use Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+    end
+  end
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
