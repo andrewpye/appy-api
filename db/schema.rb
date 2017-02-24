a7d2dab1fddaf39cf7c584c42b91a58001378364
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-02-24 09:57:11 +0200

    Add columns to users table. Add seed users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223074516) do
+ActiveRecord::Schema.define(version: 20170224075253) do
+
+  create_table "admin_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "role"
   end
 
 end
