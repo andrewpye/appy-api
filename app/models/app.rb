fc212dac2af73106f5b57621623dbd9a391aa9c9
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-03-01 07:34:04 +0200

    Destroy image files on app deletion

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,4 @@
 class App < ApplicationRecord
 	belongs_to :created_by, class_name: "User", foreign_key: "created_by_id"
-	belongs_to :image, class_name: "ImageFile", foreign_key: "image_id"
+	belongs_to :image, class_name: "ImageFile", foreign_key: "image_id", :dependent => :destroy
 end
